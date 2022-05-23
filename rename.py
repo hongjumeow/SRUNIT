a77d76f55829e18d3d1c9a0670b4a5199f610a6d
unknown <8851218@device.hmc.co.kr>
2022-05-23 10:04:31 +0900

    add renaming file

---
+++ rename.py
@@ -0,0 +1,15 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--scene', type=str, default='Scene01')
+parser.add_argument('--dataroot', type=str, default='./')
+
+opt = parser.parse_args()
+file = os.listdir(opt.dataroot)
+
+for f in file:
+    source = opt.dataroot + f
+    target = source.replace('rgb', opt.scene)
+    os.rename(source, target)
+
